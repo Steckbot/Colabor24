a1c0cdd6d30222bbe3bd881027cf198f9c482138
Steckbot <168639487+Steckbot@users.noreply.github.com>
2024-05-16 12:09:43 +0200

    Add files via upload

---
+++ 07_Grid 3/sketch.js
@@ -0,0 +1,69 @@
+let anzahl = 150;
+let rectHeight
+let rectWidth;
+// let sliderX
+
+function setup() {
+  createCanvas(600, 600);
+  noStroke();
+  fill(0);
+  frameRate(1)
+
+
+  // sliderX = createSlider(2,20);
+
+}
+
+function draw() {
+  background(255);
+  let startX = 0;
+
+    // x = sliderX.value();
+
+  for (let i = 0; i < anzahl; i++){
+    rectHeight = random (5,100);
+    rectWidth = random(5,200);
+
+    // rectHeight = 10 * sin(frameCount * 0.001);
+    // rectWidth = 10 * sin(frameCount * 0.001);
+    
+    // rectHeight = 20;
+    // rectWidth = 20;
+
+
+
+    // rectHeightAbstand = random (2,50);
+        // rectHeightAbstand = 10 * sin(frameCount * 0.001);
+        // rectHeightAbstand = 50;
+
+
+
+
+      for (let j = 1; j < anzahl/2; j++){
+      rect(startX, j*rectHeight*2-60, rectWidth, rectHeight);
+
+    }       
+   startX = startX + rectWidth
+
+  }
+
+
+
+}
+
+
+// let sliderX
+// let sliderY
+
+
+//   sliderX = createSlider(20,20);
+//   sliderY = createSlider(20,50);
+
+
+//   x = sliderX.value();
+
+//     y = sliderY.value();
+
+//     for (let j = 0; j < anzahl; j++){
+//     rect(i* x, j*y*2, x, y);
+    
